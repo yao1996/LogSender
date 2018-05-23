685cbbd4cbfdd6543246458077fdba9cfc1921ba
YaoKeqi <yaokeqi1996@outlook.com>
2018-05-23 13:16:39 +0800

    发送log

--- src/main/java/info/ykqfrost/Main.java
+++ src/main/java/info/ykqfrost/Main.java
@@ -3,25 +3,46 @@ package info.ykqfrost;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
+
 /**
  * @author Yao Keqi
  * @date 2018/4/16
  */
-public class Main {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+public class Main implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static boolean flag = true;
+    private static int errorNum = 0;
+    private static int infoNum = 0;
+    private static int totalNum = 0;
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            if (i%2==0) {
-                LOGGER.info("hello storm " + i);
+        Runnable runnable = new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Main.flag = false;
+            }
+        };
+        new Thread(runnable).start();
+        new Thread(new Main()).start();
+    }
+
+    public void run() {
+        Random random = new Random();
+        while (flag) {
+            if (random.nextInt(5) < 2) {
+                logger.info(String.valueOf(infoNum));
+                infoNum++;
             } else {
-                LOGGER.error("error sink " + i);
+                logger.error(String.valueOf(errorNum));
+                errorNum ++;
             }
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            totalNum ++;
         }
+        logger.debug(String.valueOf(totalNum));
     }
 }
